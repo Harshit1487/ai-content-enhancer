83e14f541548b88b082ff4e7d9c8a19779db659e
Harshit Jain <Harshit.Jain@triconinfotech.com>
2026-07-08 11:32:39 +0530

    added router for lead-score

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 
 from fastapi import FastAPI
-from app.routes.content import router
+from app.routes.content import router as content_router
+from app.routes.lead_scoring import router as lead_scoring_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(content_router)
+app.include_router(lead_scoring_router)
 
 @app.get("/")
 def health():
